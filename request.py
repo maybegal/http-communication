11071459014b8642b2f876098eb93db10d5cd77e
maybegal <galshichrur@gmail.com>
2024-12-06 18:09:31 +0200

    add to HTTPRequest keep_alive functions

--- request.py
+++ request.py
@@ -31,3 +31,26 @@ class HTTPRequest:
 
         # Return request line, headers, a blank line, and body in raw bytes
         return request_line + encoded_headers + self.line_break + self.body
+
+    def is_keep_alive(self) -> bool:
+        """Determines if the HTTP request is using a keep-alive connection."""
+        connection_value = self.headers.get("Connection", "")
+        return connection_value == "Keep-Alive"
+
+    def get_timeout(self) -> int:
+        if self.is_keep_alive():
+            keep_alive_value = self.headers.get("Keep-Alive", "")
+            for param in keep_alive_value.split(", "):
+                key, _, value = param.strip().partition("=")
+                if key == "timeout" and value.isdigit():
+                    return int(value)
+        return 0
+
+    def get_max_request(self) -> int:
+        if self.is_keep_alive():
+            keep_alive_value = self.headers.get("Keep-Alive", "")
+            for param in keep_alive_value.split(", "):
+                key, _, value = param.strip().partition("=")
+                if key == "max" and value.isdigit():
+                    return int(value)
+        return 0
