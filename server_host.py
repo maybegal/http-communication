c4b86a97302454f11cb33464695c58c121ec42f2
maybegal <galshichrur@gmail.com>
2024-12-06 16:43:39 +0200

    add last client to HTTPServer class to add send and close methods.

--- server_host.py
+++ server_host.py
@@ -8,29 +8,35 @@ from response import HTTPResponse
 @dataclass
 class HTTPServer:
     address: tuple[str, int]
-    buffer: int = 1024
     server = socket.socket()
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    last_client: tuple[socket, tuple[str, int]] = ()
+    buffer: int = 1024
 
     def host(self):
         """Binds the server to the server address, listens for connections"""
         self.server.bind(self.address)
         self.server.listen()
-        print(f"Server listening on {self.address[0]}:{self.address[1]}.\n")
+        print(f"Server listening on {self.address[0]}:{self.address[1]}.\n\n")
 
-    def receive(self) -> tuple[HTTPRequest, socket]:
+    def receive(self) -> HTTPRequest:
         """Handles client connection, receives HTTP request from client."""
-        client, client_address = self.server.accept()
+        client_socket, client_address = self.server.accept()
+        self.last_client = client_socket, client_address
         print(f"HTTP request received from {client_address[0]}:{client_address[1]}.")
-        request: bytes = client.recv(self.buffer)
 
-        return load_request(request), client
+        request: bytes = client_socket.recv(self.buffer)
 
+        return load_request(request)
 
-def close(response: HTTPResponse, client: socket):
-    """Sends back to client HTTP response that responds to the request, closes connection."""
-    client.send(response.dump())
-    print(f"Successfully sent HTTP response.\n")
+    def send(self, response: HTTPResponse):
+        """Sends back to client HTTP response that responds to the request"""
+        client_socket = self.last_client[0]
+        client_socket.send(response.dump())
+        print(f"Successfully sent HTTP response to {self.last_client[1][0]}:{self.last_client[1][1]}.")
 
-    # Close connection
-    client.close()
+    def close(self):
+        """Closes last client connection"""
+        client_socket = self.last_client[0]
+        client_socket.close()
+        print(f"Closed client connection at {self.last_client[1][0]}:{self.last_client[1][1]}.\n\n")
