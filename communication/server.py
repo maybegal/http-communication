8f8af7d19bf4632a73bb938f5857c944ffbe512e
maybegal <galshichrur@gmail.com>
2024-12-06 13:08:49 +0200

    update server.py remove repond func, add get example funcs, use HTTPServer and HTTPEndpoint list in main func.

--- communication/server.py
+++ communication/server.py
@@ -4,23 +4,27 @@ import socket
 
 from response import HTTPResponse
 from request import HTTPRequest
-from utils import load_request
+from endpoint import HTTPEndpoint
+from server_host import HTTPServer
 
 
-IP = "127.0.0.1"
-PORT = 3000
-ADDRESS = (IP, PORT)
-BUFFER = 4096
-
-
-def respond(http_request: HTTPRequest) -> HTTPResponse:
-    """Generate a generic HTTP response based on the given HTTP request."""
-    body = "<html><body><h1>Hello</h1></body></html>"
+def get_home(request: HTTPRequest) -> HTTPResponse:
+    body = "<html><body><h1>Home page</h1></body></html>"
 
     return HTTPResponse(
-        version=http_request.version,
-        status=200,
-        message="OK",
+        headers={
+            "Content-Length": str(len(body)),
+            "Content-Type": "text/html",
+            "Connection": "Closed"
+        },
+        body=body.encode()
+    )
+
+
+def get_pricing(request: HTTPRequest) -> HTTPResponse:
+    body = "<html><body><h1>Home page</h1></body></html>"
+
+    return HTTPResponse(
         headers={
             "Content-Length": str(len(body)),
             "Content-Type": "text/html",
@@ -33,39 +37,12 @@ def respond(http_request: HTTPRequest) -> HTTPResponse:
 def main() -> None:
     """Hosts a server to communicate with a client through receiving HTTP request and sending HTTP response."""
     # Start a TCP server to listen for client connections
-    server = socket.socket()
-    server.bind(ADDRESS)
-    server.listen()
+    server = HTTPServer(("127.0.0.1", 3000))
 
-    print(f"Server listening on {ADDRESS[0]}:{ADDRESS[1]}.")
-
-    while True:
-        try:
-            # Accept a new client connection
-            client, client_address = server.accept()
-
-            # Receive from client
-            request: bytes = client.recv(BUFFER)
-            print(f"Received HTTP request:\n\033[92m{request.decode()}\033[39;49m\n")
-
-            # Parse HTTP request
-            http_request: HTTPRequest = load_request(request)
-
-            # Generate a response
-            http_response: HTTPResponse = respond(http_request)
-
-            # Send HTTP response back to client
-            encoded_response = http_response.dump()
-
-            client.send(encoded_response)
-            print("Successfully sent back HTTP response to request.")
-
-            # Close connection
-            client.close()
-            print("Connection closed.")
-
-        except Exception as e:
-            print(f"Error handling client connection: {e}")
+    endpoints = [
+        HTTPEndpoint("/home", "GET", get_home),
+        HTTPEndpoint("/pricing", "GET", get_pricing)
+    ]
 
 
 if __name__ == '__main__':
